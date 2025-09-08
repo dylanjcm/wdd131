951e953562cdaa30904fefacc862e9ef098c3b5b
dylanjcm <dylanjanesky69@gmail.com>
2025-09-07 22:56:55 -0300

    finished most of this week assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+const year = document.querySelector("#currentyear");
+const today = new Date();
+
+year.innerHTML = `©${today.getFullYear()}`;
+
+const lastMod = document.querySelector("#lastModified");
+const lastModified = new Date(document.lastModified);
+lastMod.innerHTML = `Last Modified: ${lastModified.toLocaleString()}`;
